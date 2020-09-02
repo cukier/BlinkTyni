9be0ce25732558951dbe9e28417ddece2be8d5d1
cukier <mauriciocukier@gmail.com>
2020-09-02 17:23:53 -0300

    Makefile
    Signed-off-by: cukier <mauriciocukier@gmail.com>

---
+++ src/main.c
@@ -0,0 +1,33 @@
+/*
+ * main.c
+ *
+ *  Created on: 1 Sep 2020
+ *      Author: mauricio
+ */
+
+#include "sys.h"
+
+//#define AT90_UART
+//#include "uart.h"
+
+#include <avr/io.h>
+//#include <avr/interrupt.h>
+#include <util/delay.h>
+
+int main(void) {
+	DDRD |= (1 << 0);
+
+//	uart0_init(UART_BAUD_SELECT(BAUD, F_CPU));
+//	uart_init(UART_BAUD_SELECT(BAUD, F_CPU));
+//	unsigned long int i;
+	_delay_ms(500);
+//	sei();
+
+	while (1) {
+		PORTD ^= (1 << 0);
+//		uart_printf("hello\n");
+		_delay_ms(1500);
+	}
+
+	return 0;
+}
